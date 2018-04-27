9374d80857a6ac2fb0a479e1c8944f0723b5ef15
gaoZhiH <35756342+gaoZhiH@users.noreply.github.com>
2018-04-27 10:25:09 +0800

    Update Test.java

--- src/com/Test.java
+++ src/com/Test.java
@@ -5,7 +5,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println(112);
-        System.out.println(23123);
+        System.out.println(23123); System.out.println(231222223);
+
     }
 
 }
